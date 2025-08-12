2b629c6d8137ae8c03c36f16dde0807092bb3f95
Borges00x <160251846+Borges00x@users.noreply.github.com>
2025-08-12 14:28:30 +0000

    Desafio Nivel Mestre Correção

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -1,29 +1,29 @@
 #include <stdio.h>
 #include <string.h>
 
-void main () {
+int main () {
 
     // Variáveis que recebem os valores da primeira carta
 
-    char estado[] = "RJ";
-    char codigo[20] = "01";
-    char cidade[50] = "Rio de Janeiro";
-    unsigned long int populacao = 16055;
-    float area = 1.200;
-    float pib = 1.150;
-    int pontosturisticos = 100;
+    char estado[3];
+    char codigo[20];
+    char cidade[50];
+    unsigned long int populacao = 0;
+    float area = 0;
+    float pib = 0;
+    int pontosturisticos = 0;
     float densidade, pibPerCapita;
     float superpoder = 0;
 
     // Variáveis que recebem os valores da segunda carta
 
-    char estado2[] = "BH";
-    char codigo2[20] = "02";
-    char cidade2[50] = "Bahia";
-    unsigned long int populacao2 = 14141;
-    float area2 = 564;
-    float pib2 = 138;
-    int pontosturisticos2 = 13;
+    char estado2[3];
+    char codigo2[20];
+    char cidade2[50];
+    unsigned long int populacao2 = 0;
+    float area2 = 0;
+    float pib2 = 0;
+    int pontosturisticos2 = 0;
     float densidade2, pibPerCapita2;
     float superpoder2 = 0;
 
@@ -33,6 +33,36 @@ void main () {
     int atributo = 0, atributo2 = 0;
 
     float valor, valor2;
+
+    // Aqui as váriaveis recebem os valores da primeira carta
+
+    printf("Digite as informações de duas cartas:\n");
+
+    printf("Preencha as informações da primeira carta:\n");
+
+    printf("Digite uma letra de 'A' a 'H' que simbolize o nome da Cidade:\n");
+    scanf("%s", estado);
+
+    printf("Digite o código da carta de 01 a 04: \n");
+    scanf("%s", codigo);
+
+    getchar();
+
+    printf("Digite o nome da cidade: \n");
+    fgets(cidade, 20, stdin);
+    cidade[strcspn(cidade, "\n")] = 0;
+
+    printf("Digite o número da população da cidade: \n ");
+    scanf("%lu", &populacao);
+
+    printf("Digite a área da cidade em quilômetros quadrados: \n ");
+    scanf("%f", &area);
+
+    printf("Digite o pib(Produto Interno Bruto) da cidade: \n");
+    scanf("%f", &pib);
+
+    printf("Digite a quantidade de pontos turisticos que existe na cidade: \n");
+    scanf("%d", &pontosturisticos);
 
     // Calculo da densidade e Pib Per Capita e Super Poder da carta 1
 
@@ -48,12 +78,41 @@ void main () {
         pib = 10;
     }
 
-        densidade = populacao / area;
+        densidade = (float)populacao / area;
 
         pibPerCapita = (pib * 1000000000.0F) / populacao;
 
         superpoder = (float)populacao + area + pib +
         (float)pontosturisticos + pibPerCapita + (1.0/densidade); 
+
+    
+    // Aqui as váriaveis recebem os valores da segunda carta
+
+    printf("Preencha as informações da segunda carta:\n");
+
+    printf("Digite uma letra de 'A' a 'H' que simbolize o nome da Cidade:\n");
+    scanf("%s", estado2);
+
+    printf("Digite o código da carta de 01 a 04: \n");
+    scanf("%s", codigo2);
+
+    getchar();
+
+    printf("Digite o nome da cidade: \n");
+    fgets(cidade2, 20, stdin);
+    cidade2[strcspn(cidade2, "\n")] = 0;
+
+    printf("Digite o número da população da cidade: \n ");
+    scanf("%d", &populacao2);
+
+    printf("Digite a área da cidade em quilômetros quadrados: \n ");
+    scanf("%f", &area2);
+
+    printf("Digite o pib(Produto Interno Bruto) da cidade: \n");
+    scanf("%f", &pib2);
+
+    printf("Digite a quantidade de pontos turisticos que existe na cidade: \n");
+    scanf("%d", &pontosturisticos2); 
 
     // Calculo da densidade e Pib Per Capita e Super Poder da carta 2
 
@@ -69,7 +128,7 @@ void main () {
         pib2 = 10;
     }
 
-        densidade2 = populacao2 / area2;
+        densidade2 = (float)populacao2 / area2;
 
         pibPerCapita2 = (pib2 * 1000000000.0F) / populacao2;
 
@@ -149,7 +208,7 @@ void main () {
             printf("Houve empate!\n");
         }        
             valor = densidade;
-            valor2 = densidade;
+            valor2 = densidade2;
     break;
     default:
         printf("Número inserido inválido!\n");
@@ -235,9 +294,9 @@ void main () {
                 printf("Carta 1 - Nome: %s (%s), Atributo: Densidade populacional %.2f hab/km² \n", cidade, estado, densidade);
                 printf("Carta 2 - Nome: %s (%s), Atributo: Densidade populacional %.2f hab/km² \n", cidade2, estado2, densidade2);
             if (densidaderesult = densidade < densidade2) {
-                printf("Resultado: Carta 1 (%s) Venceu! \n", (carta - densidaderesult), cidade);
+                printf("Resultado: Carta 1 (%s) Venceu! \n", cidade);
             } else if (densidaderesult = densidade > densidade2) {
-                printf("Resultado: Carta 2 (%s) Venceu! \n", (carta - densidaderesult), cidade2);
+                printf("Resultado: Carta 2 (%s) Venceu! \n", cidade2);
             } else {
                 printf("Houve empate!\n");
             }        
